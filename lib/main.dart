1127a83b3b47dfd72fed6f71ea7f7b1d8a757c19
mrpolad <ramazanlipolad@gmail.com>
2023-09-29 20:41:33 +0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,10 @@ final List<String> taskList = [];
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Task', home: HomePage());
+    return const MaterialApp(
+      title: 'Task',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
